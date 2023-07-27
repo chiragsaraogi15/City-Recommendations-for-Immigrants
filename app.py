d6b85dae6ae4c38d5b3683ce53d0e677c24acdce
Chirag Saraogi <chiragsaraogi15@gmail.com>
2023-07-27 09:56:30 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -15,8 +15,58 @@ nltk.download('averaged_perceptron_tagger')
 # Your remaining code for the recommendations model
 stop_words = set(stopwords.words('english'))
 
-st.markdown("# Welcome to My Recommendations App")
-st.markdown("**Hello there!** This is *my Streamlit app*.")
+# heading and description text 
+st.markdown("# Welcome to My Immigrant USA City Recommendations Website")
+st.markdown("United States of America, a dream country for many immigrants coming from different parts of the world are often overwhelmed by the number of opportunities and amazing cities to choose from.")
+
+
+# Step 1: Take input for user's profession name
+user_profession = st.text_input("Enter your profession name:")
+
+# Step 2: taking weather input preferences
+coldest_temp = None
+hottest_temp = None
+
+winter_temp_mapping = {
+    "1. Not cold at all (above 60°F)": 60,
+    "2. Slightly cool (50-60°F)": 50,
+    "3. Little cold (40-50°F)": 40,
+    "4. Very cold (30-40°F)": 30,
+    "5. Extremely cold (below 30°F)": 20
+}
+
+summer_temp_mapping = {
+    "1. Not hot at all (below 65°F)": 65,
+    "2. Slightly hot (65 - 70°F)": 70,
+    "3. Little hot (70 - 80°F)": 80,
+    "4. Very hot (80 - 90°F)": 90,
+    "5. Extreme hot (above 90°F)": 100
+}
+
+# User Input: Coldest Temperature Comfortable With
+st.subheader("How do you prefer your winters to be?")
+coldest_temp = st.selectbox("Select your preference:", list(winter_temp_mapping.keys()))
+
+# User Input: Hottest Temperature Comfortable With
+st.subheader("How do you prefer your summers to be?")
+hottest_temp = st.selectbox("Select your preference:", list(summer_temp_mapping.keys()))
+
+# Step 3: Take input for the country from the user
+countries = ['Mexico', 'China', 'India', 'Philippines', 'Dominican Republic', 'Cuba', 'Vietnam', 'El Salvador',
+             'Korea', 'Jamaica', 'Brazil', 'Haiti', 'Colombia', 'Pakistan', 'Iraq', 'Bangladesh', 'Nigeria',
+             'Ethiopia', 'Canada', 'Iran', 'Guatemala', 'United Kingdom', 'Nepal', 'Other Countries']
+
+# User Input: Country Selection
+st.subheader("Choose your country from the list:")
+choice = st.selectbox("Select your country:", countries)
+
+# Display the inputs
+st.subheader("Your inputs:")
+st.write("Profession Name:", user_profession)
+st.write("You are comfortable with a coldest temperature of", winter_temp_mapping[coldest_temp], "Fahrenheit.")
+st.write("You are comfortable with a hottest temperature of", summer_temp_mapping[hottest_temp], "Fahrenheit.")
+st.write("Which country are you from:", choice)
+
 
 def get_pos_tag(token):
     pos_tag = nltk.pos_tag([token])[0][1]
@@ -47,111 +97,9 @@ def jaccard_similarity(set1, set2):
     
 df = pd.read_pickle('tagged_data.pkl')
 
-# Step 1: Take input for user's profession name
-user_profession = st.text_input("Enter your profession name:")
-
-coldest_temp = None
-hottest_temp = None
-
-winter_temp_mapping = {
-    "1. Not cold at all (above 60°F)": 60,
-    "2. Slightly cool (50-60°F)": 50,
-    "3. Little cold (40-50°F)": 40,
-    "4. Very cold (30-40°F)": 30,
-    "5. Extremely cold (below 30°F)": 20
-}
-
-summer_temp_mapping = {
-    "1. Not hot at all (below 65°F)": 65,
-    "2. Slightly hot (65 - 70°F)": 70,
-    "3. Little hot (70 - 80°F)": 80,
-    "4. Very hot (80 - 90°F)": 90,
-    "5. Extreme hot (above 90°F)": 100
-}
-
-'''
-
-# Step 2: Take input for coldest temperature comfortable with
-while True:
-    if coldest_temp is None:
-        print("How do you prefer your winters to be?")
-        for option in winter_temp_mapping:
-            print(option)
-        user_choice = input("Enter the corresponding number for your preference: ")
-
-        if user_choice.isdigit() and int(user_choice) in range(1, len(winter_temp_mapping) + 1):
-            coldest_temp = winter_temp_mapping[list(winter_temp_mapping.keys())[int(user_choice) - 1]]
-            print("You are comfortable with a coldest temperature of", coldest_temp, "Fahrenheit.")
-            break  # Corrected the break statement location to exit the loop
-        else:
-            print("Invalid input. Please select a valid option.")
-
-# Step 3: Take input for hottest temperature comfortable with
-while True:
-    if hottest_temp is None:
-        print("How do you prefer your summers to be?")
-        for option in summer_temp_mapping:
-            print(option)
-        user_choice = input("Enter the corresponding number for your preference: ")
-
-        if user_choice.isdigit() and int(user_choice) in range(1, len(summer_temp_mapping) + 1):
-            hottest_temp = summer_temp_mapping[list(summer_temp_mapping.keys())[int(user_choice) - 1]]
-            print("You are comfortable with a hottest temperature of", hottest_temp, "Fahrenheit.")
-            break  # Corrected the break statement location to exit the loop
-        else:
-            print("Invalid input. Please select a valid option.")
-
-'''
-# Step 4: Take input for the country from the user
-countries = ['Mexico', 'China', 'India', 'Philippines', 'Dominican Republic', 'Cuba', 'Vietnam', 'El Salvador',
-             'Korea', 'Jamaica', 'Brazil', 'Haiti', 'Colombia', 'Pakistan', 'Iraq', 'Bangladesh', 'Nigeria',
-             'Ethiopia', 'Canada', 'Iran', 'Guatemala', 'United Kingdom', 'Nepal', 'Other Countries']
-
-'''
-while True:
-    print("Choose your country from the list:")
-    for i, country in enumerate(countries, start=1):
-        print(f"{i}. {country}")
-
-    try:
-        choice = int(input("Enter the number corresponding to your country: "))
-        if 1 <= choice <= len(countries):
-            country = countries[choice - 1]
-            break
-        else:
-            print("Invalid choice. Please enter a valid number.")
-    except ValueError:
-        print("Invalid input. Please enter a numeric value.")
-# Display the variables
-print("Your inputs:")
-print("Profession Name:", user_profession)
-print("You are comfortable with a coldest temperature of", coldest_temp, "Fahrenheit.")
-print("You are comfortable with a hottest temperature of", hottest_temp, "Fahrenheit.")
-print("Which country are you from:", country)
-'''
-
-# User Input: Coldest Temperature Comfortable With
-st.subheader("How do you prefer your winters to be?")
-coldest_temp = st.selectbox("Select your preference:", list(winter_temp_mapping.keys()))
-
-# User Input: Hottest Temperature Comfortable With
-st.subheader("How do you prefer your summers to be?")
-hottest_temp = st.selectbox("Select your preference:", list(summer_temp_mapping.keys()))
-
-# User Input: Country Selection
-st.subheader("Choose your country from the list:")
-choice = st.selectbox("Select your country:", countries)
-
-# Display the inputs
-st.subheader("Your inputs:")
-st.write("Profession Name:", user_profession)
-st.write("You are comfortable with a coldest temperature of", winter_temp_mapping[coldest_temp], "Fahrenheit.")
-st.write("You are comfortable with a hottest temperature of", summer_temp_mapping[hottest_temp], "Fahrenheit.")
-st.write("Which country are you from:", choice)
-
 
 selected_columns = ['SAFETY_INDEX', 'COST_OF_LIVING_INDEX', 'WINTER_COLDEST_TEMP',
-                    'SUMMER_HOTTEST_TEMP', 'PROFESSION', 'TOT_EMP', 'H_MEAN', 'A_MEAN', 'TAGS','EXPANDED_TAGS',country]
+                    'SUMMER_HOTTEST_TEMP', 'PROFESSION', 'TOT_EMP', 'H_MEAN', 'A_MEAN', 'TAGS','EXPANDED_TAGS',choice]
 
 filtered_df = df[selected_columns]
 
@@ -182,11 +130,12 @@ filtered_df = filtered_df.sort_values(by='Profession_Score', ascending=False) \
                          .groupby('CITY').head(1) \
                          .sort_index()
 
-filtered_df = filtered_df.sort_values(by=['TOT_EMP', 'A_MEAN', country, 'SAFETY_INDEX', 'COST_OF_LIVING_INDEX'],
+filtered_df = filtered_df.sort_values(by=['TOT_EMP', 'A_MEAN', choice, 'SAFETY_INDEX', 'COST_OF_LIVING_INDEX'],
                                       ascending=[False, False, False, False, True])
 
 
-desired_columns = ['PROFESSION', 'TOT_EMP', 'A_MEAN', country, 'WINTER_COLDEST_TEMP', 'SUMMER_HOTTEST_TEMP']
+desired_columns = ['PROFESSION', 'TOT_EMP', 'A_MEAN', choice, 'WINTER_COLDEST_TEMP', 'SUMMER_HOTTEST_TEMP']
 filtered_df = filtered_df[desired_columns]
 
-filtered_df.head(5)
+st.subheader("Top 5 Recommendations:")
+st.table(filtered_df.head(5))
