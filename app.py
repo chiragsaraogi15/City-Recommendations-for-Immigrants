a5fd55db9d896efe04eb52dca816f6c878556454
Chirag Saraogi <chiragsaraogi15@gmail.com>
2023-07-30 07:48:42 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -213,8 +213,8 @@ if st.button("Submit"):
     }
 
     .card-img-container {
-        width: 250px;
-        height: 250px;
+        width: 200px;
+        height: 200px;
         overflow: hidden;
     }
 
